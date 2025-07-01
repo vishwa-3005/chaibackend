e8a59ce40a05775b857738cdf3c7db243f2ec0a6
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-07-01 15:07:32 +0530

    Implement update playlist functionality: add video to playlist with validation and error handling

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -172,5 +172,48 @@ const deletePlayList = asyncHandler(async (req, res) => {
 });
 //6.update playlist
 const updatePlaylist = asyncHandler(async (req, res) => {
-  const { playListId } = req.params;
+  const { playListId, videoId } = req.params;
+  if (!playListId || !videoId) {
+    throw new ApiError(
+      400,
+      "video id or playlist id is missing from request params"
+    );
+  }
+  if (!mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "Invalid video ID format");
+  }
+  if (!mongoose.Types.ObjectId.isValid(playListId)) {
+    throw new ApiError(400, "Invalid playlist ID format");
+  }
+  const playList = await Playlist.findById(playListId);
+  if (!playList) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  if (playList.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(
+      403,
+      "Unauthorization: you are not owner of this playlist to add video to it"
+    );
+  }
+
+  //Mongoose ObjectId instances come with a .equals() method specifically for value-based comparisons.
+  const isVideoExists = playList.videos.some((v) => v.equals(videoId)); // t/f
+  if (isVideoExists) {
+    throw new ApiError(400, "Video already exists in playlist");
+  }
+
+  playList.videos.push(videoId); // Mongoose will cast it to ObjectId
+
+  await playList.save();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playList, "Video added to playlist successfully")
+    );
 });
