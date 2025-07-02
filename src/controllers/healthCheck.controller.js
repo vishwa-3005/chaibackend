0c6bd4f492d8fb429545a735d89f1c7db3b97468
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-07-03 01:32:41 +0530

    Refactor healthCheck response to use ApiResponse for consistency

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,16 +1,21 @@
 //TODO: build a healthcheck response that simply returns the OK status as json with a message
 import mongoose from "mongoose";
-
+import { ApiResponse } from "../utils/apiResponse.js";
 const healthCheck = async (req, res) => {
   const dbStatus =
     mongoose.connection.readyState === 1 ? "connected" : "disconnected";
 
-  res.status(200).json({
-    status: "ok",
-    uptime: process.uptime(), // server uptime in seconds
-    db: dbStatus,
-    timestamp: new Date().toISOString(),
-  });
+  res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        uptime: process.uptime(), // server uptime in seconds
+        db: dbStatus,
+        timestamp: new Date().toISOString(),
+      },
+      "ok!"
+    )
+  );
 };
 
 export { healthCheck };
